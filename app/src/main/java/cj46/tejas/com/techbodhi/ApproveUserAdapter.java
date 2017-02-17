4e3d56d4f53be89d8026df002b7a7fc5003f1faf
tejasgawali4 <tejas.gawali4@gmail.com>
2017-02-17 15:23:10 +0530

    Leatest Code.

---
+++ app/src/main/java/cj46/tejas/com/techbodhi/ApproveUserAdapter.java
@@ -0,0 +1,8 @@
+package cj46.tejas.com.techbodhi;
+
+/**
+ * Created by Carl_johnson on 2/9/2017.
+ */
+
+public class ApproveUserAdapter {
+}
---
+++ app/src/main/java/cj46/tejas/com/techbodhi/UserPanelViewPostDetails.java
@@ -0,0 +1,8 @@
+package cj46.tejas.com.techbodhi;
+
+/**
+ * Created by Carl_johnson on 2/16/2017.
+ */
+
+public class UserPanelViewPostDetails {
+}
